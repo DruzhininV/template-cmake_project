2c47854ee0277b8e638fb2098b0a82cb86789d6f
Vladimir Druzhinin <lVladimirDruzhininl@gmail.com>
2019-01-27 12:31:05 +0300

    gtest

---
+++ tests/testmain.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Vladimir Druzhinin on 26.01.2019.
+//
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(Test, test1) {
+    ASSERT_TRUE(true);
+}
+//int main(int argc, char** argv) {
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
